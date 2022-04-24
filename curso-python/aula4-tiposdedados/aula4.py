aad33fc5b5fb2a554a01a25f46ad8aa5a54bb16d
CaioHBastos <caio.cbastos@hotmail.com>
2022-04-24 09:04:23 -0300

    Iniciando com os tipos primitivos

---
+++ curso-python/aula4-tiposdedados/aula4.py
@@ -0,0 +1,28 @@
+"""
+Tipos de dados
+str - string -> textos 'Assim' "Assim"
+int - inteiro -> 123456, 0, -1
+float - real/ponto flutuante -> 10.50, 1.5, -10.40
+bool - boolean/lógico -> True/False
+"""
+print('Luiz', type('Luiz'))
+print(123456, type(123456))
+print(1.10, type(1.10))
+print(True, type(True))
+print(10 == 10, type(10 == 10))
+
+# TypeCasting
+print('Caio', type('Caio'), bool('Caio'))
+print('10', type('10'), type(int('10')))
+
+"""
+Cadastrar o seu nome e verificar o tipo - str
+Cadastrar a sua idade e verificar o tipo - int
+Cadastrar a sua alterar e verificar o tipo - float
+Cadastrar se é maior de idade e verificar o tipo - bool
+"""
+# String
+print('Caio', type('Caio'))
+print(24, type(24))
+print(1.70, type(1.70))
+print(24 > 18, type(24 > 18))
