0468aebdbd5710cb6eb987d7599d5405815d54f5
CaioHBastos <caio.cbastos@hotmail.com>
2022-05-04 21:30:01 -0300

    Iterando strings com while

---
+++ curso-python/aula15-estuturarepeticao/aula15-3-whilestring.py
@@ -0,0 +1,15 @@
+"""
+Interando string com while
+"""
+frase = 'o rato roeu a roupa do rei de roma'
+tamanho_frase = len(frase)
+contador = 0
+nova_string = ''
+
+while contador < tamanho_frase:
+    #  print(frase[contador], contador)
+    nova_string += frase[contador]
+    print(nova_string)
+    contador += 1
+
+print(nova_string)
