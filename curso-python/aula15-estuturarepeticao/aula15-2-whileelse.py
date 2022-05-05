73cb904b29079561948f9623a8eb90af79abc6a0
CaioHBastos <caio.cbastos@hotmail.com>
2022-05-04 21:16:29 -0300

    COntinuando com while

---
+++ curso-python/aula15-estuturarepeticao/aula15-2-whileelse.py
@@ -0,0 +1,21 @@
+"""
+While/Else
+contadores
+acumuladores
+"""
+contador = 0
+while contador <= 10:
+    print(contador)
+    contador += 1
+print('Acabou - contador')
+
+contador = 1
+acumulador = 1
+while contador <= 10:
+    print(contador, acumulador)
+
+    acumulador = acumulador + contador
+    contador += 1
+else:
+    print('Cheguei no else')
+print('Acabou - acumulador')
